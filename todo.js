2aff36a6a6b8bb1a48d1fc7e8739ee24831299b9
Demetris278 <150178664+Demetris278@users.noreply.github.com>
2024-04-18 00:20:57 +0300

    Even more baby steps !!! <steps>4</steps>
    baby made slightly more steps but still falls :(

--- todo.js
+++ todo.js
@@ -1,10 +1,7 @@
 let inputText=document.getElementById("input-text");
 let taskList=document.getElementById("task-list");
 
-
-function completeTask(e){
-    e.target.remove();
-}
+loadsaved();
 function addTask(){
     if(inputText.value!=""){
         let entry=document.createElement("entry");
@@ -12,28 +9,33 @@ function addTask(){
         let erase=document.createElement("erase");
         erase.innerHTML="X";
         entry.appendChild(erase);
-        taskList.appendChild(entry); 
+        taskList.appendChild(entry);
+        localsave();
+        alert(localstorage.getitem("localdata"));
+        
     }
     inputText.value="";
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 taskList.addEventListener("click",function(e){
     switch(e.target.tagName){
         case "ENTRY":
+            e.target.classList.toggle("completed");
+            localsave();
+            break;
+        case "ERASE":
             e.target.parentElement.remove();
+            localsave();
             break;
     }
+
 });
+
+function localsave(){
+    localStorage.setItem("data",taskList.innerHTML);
+}
+
+function loadsaved(){
+    taskList.innerHTML= localstorage.getItem("data");
+    
+}
