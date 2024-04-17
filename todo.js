259bc52bc78b44644e0e7e9b71188039ea408ba8
Demetris278 <150178664+Demetris278@users.noreply.github.com>
2024-04-17 23:48:20 +0300

    BABY made its first step ! <steps>3</steps>
    she tried to run but failed. its ok it will happen soon!

--- todo.js
+++ todo.js
@@ -1,10 +1,39 @@
 let inputText=document.getElementById("input-text");
 let taskList=document.getElementById("task-list");
 
+
+function completeTask(e){
+    e.target.remove();
+}
 function addTask(){
     if(inputText.value!=""){
-       let entry=document.createElement("entry");
+        let entry=document.createElement("entry");
         entry.innerHTML=inputText.value;
-        taskList.appendChild(entry);
+        let erase=document.createElement("erase");
+        erase.innerHTML="X";
+        entry.appendChild(erase);
+        taskList.appendChild(entry); 
     }
+    inputText.value="";
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+taskList.addEventListener("click",function(e){
+    switch(e.target.tagName){
+        case "ENTRY":
+            e.target.parentElement.remove();
+            break;
+    }
+});
